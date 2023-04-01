a55cbd7a055ea6f17388b9845bd34ad075ab45dd
ayoub batili <batiliayoub@gmail.com>
2023-03-31 19:23:26 -0700

    0x01-variables_if_else_while

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,24 +7,16 @@
  */
 int main(void)
 {
-	char ch = 'a';
+        char ch = 'a';
 
-	while (ch <= 'z')
-	{
-		if (ch != 'e' && ch != 'q')
-		{
-			putchar(ch);
-		}
-		ch++;
-	}
-	putchar('\n');
-	return (0);
+        while (ch <= 'z')
+        {
+                if (ch != 'e' && ch != 'q')
+                {
+                        putchar(ch);
+                }
+                ch++;
+        }
+        putchar('\n');
+        return (0);
 }
-Footer
-© 2023 GitHub, Inc.
-Footer navigation
-Terms
-Privacy
-Security
-Status
-Docs
